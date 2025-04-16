975fd90c29ea1792021152ff208d4eaef4d378ad
rjf1924 <149185219+rjf1924@users.noreply.github.com>
2025-04-16 15:47:39 -0400

    Fixed clientIP again

--- main.py
+++ main.py
@@ -98,10 +98,14 @@ def get_server_ip():
     return gws['default'][netifaces.AF_INET][0]
 
 
-def get_my_ip():
-    hostname = socket.gethostname()
-    return socket.gethostbyname(hostname)
 
+def get_my_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(("8.8.8.8", 80))
+        return s.getsockname()[0]
+    finally:
+        s.close()
 
 def get_config():
     with open("config.json", "r") as f:
