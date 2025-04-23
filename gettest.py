8cf453f0b8b930f37773be91f63e517cb38463c3
rjf1924 <149185219+rjf1924@users.noreply.github.com>
2025-04-23 12:07:28 -0400

    refactoring + bug fix

--- gettest.py
+++ gettest.py
@@ -8,11 +8,12 @@ if __name__ == '__main__':
     import network
 
     network.subscribe("robert/command", on_msg)
-
+    network.start_loop()
     for address, frame in network.get_next_frame():
         # do computation
+        print(address, frame)
 
-    network.start_loop()
+
 
     while True:
         pass
